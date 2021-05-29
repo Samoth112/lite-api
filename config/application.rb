8dac6478e639f3098d996435e983db855a324e50
Samoth112 <tc.thomascho@gmail.com>
2021-05-29 19:54:06 -0400

    updated cors

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module LiteApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'http://localhost:3001'
+    'Access-Control-Allow-Origin' => 'https://lite-api.herokuapp.com'
   }
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mybiblios.herokuapp.com', 'http://localhost:3001'
+    origins 'https://lite-api.herokuapp.com', 'http://localhost:3001'
     resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options], credentials: true
   end
 end
